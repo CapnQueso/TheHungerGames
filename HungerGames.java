0e804df0fa82b91c2f2f38f43cea0704a2eb24d7
CapnQueso <ejrosas@outlook.com>
2025-06-19 00:06:03 -0700

    Update with vision, as well as adding more complex logic to intro sequence

--- HungerGames.java
+++ HungerGames.java
@@ -10,10 +10,10 @@ import java.util.Scanner;
 public class HungerGames {
     // Constants
     public static ArrayList<String> tributes = new ArrayList<>();
-    public static ArrayList<String> players = new ArrayList<>();
+    public static ArrayList<Integer> players = new ArrayList<>(); //list of which tributes are players(1) and which are bots(0)
     public static String[] items = {"Sword", "Shield", "Bow", "Food", "Water"}; //make txt file
     public static String[] deaths = {"stabbed", "bashed", "shot", "poisoned", "drowned"};
-    public static ArrayList<String> kills = new ArrayList<>();
+    public static ArrayList<Integer> kills = new ArrayList<>();
     public static int numTributes = 0;
     public static int day = 1;
 
@@ -27,14 +27,30 @@ public class HungerGames {
         System.out.print("Enter the number of tributes: ");
         numTributes = input.nextInt();
         
-        //cycle through the number of tributes and get their names
+        //cycle through the number of tributes and get their names, and initialize kills
+        //also determine if the tribute is a player or a bot
         for(int i = 0; i < numTributes; i++) {
             System.out.print("Enter the name of tribute " + (i + 1) + ": ");
             String name = input.next();
             tributes.add(name);
+            kills.add(0);
+            System.out.print("Is this tribute a player? (yes/no): ");
+            String isPlayer = input.next();
+            if (isPlayer.toLowerCase().substring(0, 1).equals("y")) {
+                players.add(1);
+            } else {
+                players.add(0);
+            }
         }
+        //print initial test information
         System.out.println("Tributes: " + tributes);
+        System.out.println("Players: " + players);
         
+        //intro
+        System.out.println("Welcome to the Hunger Games!");
+        System.out.println("There are " + numTributes + " tributes in the arena");
+        System.out.println("May the odds be ever in your favor!");
+
         //simulate days until only one tribute remains
         while(numTributes > 1){
             simulateDay();
@@ -50,23 +66,42 @@ public class HungerGames {
     }
 
     /**
+     * Simulate the bloodbath at the start of the Hunger Games.
+     * This method will be called once at the beginning of the simulation.
+     * It selects tributes to die in the initial chaos.
+     */
+    public static void simulateBloodBath(){
+
+
+    }
+
+    /**
      * Simulate a day in the hunger games
      * Uses random events to determine if tributes die, recieve items, or kill others.
      * This method will be called repeatedly until only one tribute remains.
      * PvE will be sent back to the player to decide what to do with the items.
      */
     public static void simulateDay() {
+        //order:
+        //events
+        //sponsors
+        //scavenge
+        //combat
+        //announce deaths
+        //update variables
+        //check for winner
+        //if no winner, repeat
 
-
-    }
+        }
 
     /** 
      * Simulate a turn in combat
      * This method will be called when two tributes are in combat.
      */
     public static void combatTurn(){
-
-
+        //pokemon style combat
+        //each combat turn, players choice(s) will be given a score, 
+        //the tribute with the highest score of the engagement will be the victor
     }
 
     /** Simulate combat between two tributes
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Play a fun text adventure spin on The Hunger games
 featuring pokemon-style turn based combat, bots so you can play without your friends, and a lot of silly little things for you to do.
 
 my first ever big project btw :)
+
+for questions, contact 'CapnQueso' on discord
